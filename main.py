0b57122c6f15d6c0dce8c1f95083b60fe0a97045
jean persia <jeanlpersia@gmial.com>
2023-07-22 18:55:36 -0400

    Commit  Changes to be committed: 	modified:   .gitignore 	modified:   main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-import secrets as keys
+import keys
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes\
 
 async def StartCommand(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(f'Objeto UPDATE: {update}\n\n')
     await context.bot.send_message(chat_id=update.effective_chat.id, text='Hello, check the menu for more info!!')
+
+async def HandleMessage(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'dont understand you, please use the command menu to interact with me")
+
 
 
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(keys.API_KEY).build()
     start_handler = CommandHandler('start', StartCommand)
+
+    #ASSIGNING HANDLERS
     application.add_handler(start_handler)
+    application.add_handler(MessageHandler(filters.ALL, HandleMessage))
 
+    #PULLING UPDATES
     application.run_polling()
-
